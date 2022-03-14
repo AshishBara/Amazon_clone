d71fafa30e9c60ddb0bc0c22339a6293cf68e062
Ashish <ashish@Ashishs-MacBook-Air.local>
2022-03-14 09:38:51 +0530

    initial changes

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAciZ46dMMo7oxUVcG1R0KVQ3aoqhT2YxY",
+    authDomain: "clone-09.firebaseapp.com",
+    projectId: "clone-09",
+    storageBucket: "clone-09.appspot.com",
+    messagingSenderId: "457783277667",
+    appId: "1:457783277667:web:1fa0b682da4a598702a056"
+  };
